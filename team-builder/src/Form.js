076ddd065ce06c9fa9b5d845ffb7b523e645b727
Vick Javariz <51038821+javavick@users.noreply.github.com>
2019-10-10 00:56:17 -0400

    Build Form.js component structure, add slice of state for the form data, and create a change handler function

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -1,5 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Form() {}
+function Form() {
+  // Slice of state for the form data
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    role: ""
+  });
+
+  // Change handler function
+  function changeHandler(event) {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value
+    });
+  }
+
+  // The form itself
+  return (
+    <form>
+      <label>
+        Name:
+        <input
+          id="nameInput"
+          name="name"
+          type="text"
+          onChange={changeHandler}
+        />
+      </label>
+
+      <label>
+        Email:
+        <input
+          id="emailInput"
+          name="email"
+          type="email"
+          onChange={changeHandler}
+        />
+      </label>
+
+      <label>
+        Role:
+        <select id="roleSelect" name="role" onChange={changeHandler}>
+          <option value="frontEnd">Front-End Engineer</option>
+          <option value="backEnd">Back-End Engineer</option>
+          <option value="designer">UX Designer</option>
+          <option></option>
+        </select>
+      </label>
+    </form>
+  );
+}
 
 export default Form;
