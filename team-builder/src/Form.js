f5a99b60ab7ad1f62a361dc92e1d3eeb075e8534
Vick Javariz <51038821+javavick@users.noreply.github.com>
2019-10-10 01:18:30 -0400

    Touch up Form.js structure and render it in App.js

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -18,39 +18,45 @@ function Form() {
 
   // The form itself
   return (
-    <form>
-      <label>
-        Name:
-        <input
-          id="nameInput"
-          name="name"
-          type="text"
-          onChange={changeHandler}
-        />
-      </label>
+    <div>
+      <h1>Team Member</h1>
+      <form>
+        <label>
+          Name: <span></span>
+          <input
+            id="nameInput"
+            name="name"
+            type="text"
+            onChange={changeHandler}
+          />
+        </label>
+        <br />
 
-      <label>
-        Email:
-        <input
-          id="emailInput"
-          name="email"
-          type="email"
-          onChange={changeHandler}
-        />
-      </label>
+        <label>
+          Email: <span></span>
+          <input
+            id="emailInput"
+            name="email"
+            type="email"
+            onChange={changeHandler}
+          />
+        </label>
+        <br />
 
-      <label>
-        Role:
-        <select id="roleSelect" name="role" onChange={changeHandler}>
-          <option value="frontEnd">Front-End Engineer</option>
-          <option value="backEnd">Back-End Engineer</option>
-          <option value="designer">UX Designer</option>
-          <option></option>
-        </select>
-      </label>
+        <label>
+          Role: <span></span>
+          <select id="roleSelect" name="role" onChange={changeHandler}>
+            <option value="none"></option>
+            <option value="frontEnd">Front-End Engineer</option>
+            <option value="backEnd">Back-End Engineer</option>
+            <option value="designer">UX Designer</option>
+          </select>
+        </label>
+        <br />
 
-      <button>Submit</button>
-    </form>
+        <button>Submit</button>
+      </form>
+    </div>
   );
 }
 
